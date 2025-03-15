9c34a1d901dfa068253afe008906d83c6ae06e4e
SGDilakshan <dilakshan.contact@gmail.com>
2025-03-15 12:38:16 +0530

    Update the Break and Continue in Java

--- src/loops/BreakAndContinue.java
+++ src/loops/BreakAndContinue.java
@@ -1,4 +1,15 @@
 package loops;
 
 public class BreakAndContinue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+            if (i == 8) {
+                break;
+            }
+        }
+    }
 }
