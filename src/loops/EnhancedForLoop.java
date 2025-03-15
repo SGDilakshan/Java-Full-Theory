41c091a53e5941a1bd153da548873db7df107ff2
SGDilakshan <dilakshan.contact@gmail.com>
2025-03-15 10:43:21 +0530

    Update after Enhanced for loop in java

---
+++ src/loops/EnhancedForLoop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int numbers[]={10,20,30,40,50,60,70};
+        for(int n:numbers){
+            System.out.println(n);
+        }
+    }
+}
