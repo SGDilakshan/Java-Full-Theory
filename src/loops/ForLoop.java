a5e912fa1bc1db6a5524de82df27242297b80f5b
SGDilakshan <dilakshan.contact@gmail.com>
2025-03-15 07:54:03 +0530

    Update after for loop in java

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,14 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Enter the limit:");
+        Scanner scanner = new Scanner(System.in);
+        int limit = scanner.nextInt();
+        for(int i=1;i<=limit;i++){
+            System.out.println(i);
+        }
+    }
+}
